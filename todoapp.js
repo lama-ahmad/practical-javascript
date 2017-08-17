288a0b0578274edbcc51ecb464951c30740348e9
Lama Ahmad <lamaahmad@Lamas-MacBook-Pro.local>
2017-08-17 18:35:28 -0400

    version 1 basic commands

--- todoapp.js
+++ todoapp.js
@@ -1,3 +1,10 @@
 // Array in JavaScript will store our todos
+var todos = ['item1', 'item2', 'item3'];
 
-var todos = ['item1', 'item2', 'item3']
+// Print out our todos to display them
+console.log("My todos:", todos);
+
+// Add new todos to our list
+todos.push('item4');
+
+// Changing existing todo
