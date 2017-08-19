80569d7c8a44cc1cd7ab967cdf7efb0b3fb9cdea
Lama Ahmad <lamaahmad@Lamas-MacBook-Pro.local>
2017-08-19 17:57:06 -0400

    .displayTodos clean-up

--- todoapp.js
+++ todoapp.js
@@ -3,7 +3,18 @@ var todoList = {
     todos: [],
     // Function to display todos 
     displayTodos: function() {
-        console.log(this.todos);        
+        if (this.todos.length === 0) {
+            console.log('Todo list is empty');
+        } else {
+            console.log('My Todos:');                  
+            for (var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('( )', this.todos[i].todoText);                    
+                }
+            }  
+        }         
     },
     // Function to add todos 
     addTodo: function(todoText) {
